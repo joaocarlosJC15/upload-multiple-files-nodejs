164f750693a2a8b34b5355476474157c2058e3be
Joao Carlos Alvim da Cunha <jc15joaocarlos@gmail.com>
2021-01-05 22:38:05 -0300

    frontend: Concluido o video 1

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,4 +1,6 @@
 let bytesAmount = 0
+const API_URL = "http://localhost:3000"
+const ON_UPLOAD_EVENT = "file-uploaded"
 
 const formatBytes = (bytes, decimals = 2) => {
   if (bytes === 0) return '0 Bytes';
@@ -29,8 +31,49 @@ const showSize = () => {
   updateStatus(size)
 }
 
-const onload = () => {
+const updateMessage = (message) => {
+  const msg = document.getElementById('msg')
+  msg.innerText = message
 
+  msg.classList.add('alert', 'alert-success')
+  setTimeout(() => (msg.hidden = true), 3000)
+}
+
+const showMessage = () => {
+  const urlParams = new URLSearchParams(window.location.search)
+  const serverMessage = urlParams.get('msg')
+  
+  if(!serverMessage) return;
+ 
+  updateMessage(serverMessage)
+}
+
+const configureForm = (targetUrl) => {
+  const form = document.getElementById("form")
+  form.action = targetUrl
+}
+
+const onload = () => {
+  showMessage()
+
+  const ioClient = io.connect(API_URL, { withCredentials: false })
+
+  ioClient.on("connect", (msg) => {
+    console.log("connected!", ioClient.id)
+
+    const targetUrl = API_URL + `?socketId=${ioClient.id}`
+
+    configureForm(targetUrl)
+  })
+
+  ioClient.on(ON_UPLOAD_EVENT, (bytesReceived) => {
+    console.log("received", bytesReceived)
+    bytesAmount = bytesAmount - bytesReceived
+
+    updateStatus(bytesAmount)
+  })
+
+  updateStatus(0)
 }
 
 window.onload = onload
